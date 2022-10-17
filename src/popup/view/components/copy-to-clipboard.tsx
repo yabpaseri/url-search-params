f22deacf46ffdccc40ae6006a9cdfe4e59604329
yabpaseri <yab.82.hiro@gmail.com>
2022-10-17 17:49:09 +0900

    [fix] Fixed a problem where some tooltips were not unmounted correctly.

--- src/popup/view/components/copy-to-clipboard.tsx
+++ src/popup/view/components/copy-to-clipboard.tsx
@@ -1,5 +1,5 @@
 import Tooltip from '@mui/material/Tooltip';
-import { ComponentProps, memo, useCallback, useState } from 'react';
+import { ComponentProps, memo, useCallback, useEffect, useRef, useState } from 'react';
 
 type TProps = Pick<ComponentProps<typeof Tooltip>, 'children'> & {
 	defaultTitle?: string;
@@ -12,10 +12,27 @@ export const CopyToClipboard = memo<TProps>(({ defaultTitle, text, children }) =
 		window.navigator.clipboard.writeText(text);
 		setTitle('Copied!');
 	}, [text]);
-	const handleExited = useCallback(() => setTitle(defaultTitle), [defaultTitle]);
+
+	// TransitionProps.onExited was used to return the value to default.
+	// However, I found that Tooltip is not unmounted when I override onExited.
+	// I had no choice but to substitute setTimeout.
+	//     const handleExited = useCallback(() => setTitle(defaultTitle), [defaultTitle]);
+	//     <Tooltip TransitionProps={{ onExited: handleExited }} .../>
+	const timeout = useRef<NodeJS.Timeout | null>(null);
+	const resetTimeout = useCallback(() => {
+		if (timeout.current) {
+			clearTimeout(timeout.current);
+			timeout.current = null;
+		}
+	}, []);
+	useEffect(() => () => resetTimeout(), [resetTimeout]);
+	const handleClose = useCallback(() => {
+		resetTimeout();
+		timeout.current = setTimeout(() => setTitle(defaultTitle), 200);
+	}, [defaultTitle, resetTimeout]);
 
 	return (
-		<Tooltip disableInteractive arrow title={title} onClick={handleClick} TransitionProps={{ onExited: handleExited }}>
+		<Tooltip disableInteractive arrow title={title} onClick={handleClick} TransitionProps={{ onExit: handleClose }}>
 			{children}
 		</Tooltip>
 	);

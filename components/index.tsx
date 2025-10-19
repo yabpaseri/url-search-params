7d8143196744911f4a858a91fb780902d44feac2
yabpaseri <yab.82.hiro@gmail.com>
2025-10-19 19:31:51 +0900

    :coffin: Remove unnecessary files.

--- components/index.tsx
+++
@@ -1,5 +0,0 @@
-export { ArrowTooltip } from './ArrowTooltip';
-export { CopyToClipboard } from './CopyToClipboard';
-export { FadeMotion } from './FadeMotion';
-export { OutlinedIconButton } from './OutlinedIconButton';
-export { SimpleInput } from './SimpleInput';
